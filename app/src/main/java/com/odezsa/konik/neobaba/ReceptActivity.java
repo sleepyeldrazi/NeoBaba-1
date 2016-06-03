36efadf2c3e83404b6191f6a349029775d467063
sleepyeldrazi <konik98@gmail.com>
2016-06-03 05:20:14 +0300

    i did some stuff
    i did things..

--- app/src/main/java/com/odezsa/konik/neobaba/ReceptActivity.java
+++ app/src/main/java/com/odezsa/konik/neobaba/ReceptActivity.java
@@ -1,7 +1,9 @@
 package com.odezsa.konik.neobaba;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
@@ -11,29 +13,40 @@ import android.view.View;
 import android.widget.AdapterView;
 
 import android.widget.ListView;
+
 import android.widget.Toast;
 
 
 
+public class ReceptActivity extends AppCompatActivity{
+
     ListView listView;
     DatabaseHelper myDbHelper;
-    String[] recNames = new String[2];
+    ReceptSample[] recipes;
 
     int count;
 
     int chosen[];
 
-    Integer[] imageId = new Integer[2];
-
-
-
-
     //Cursor cursor =  db.rawQuery("select * from products where prodName like '" + searchView.getQuery() + "%';" , null);
 
 
+
+    public String[] getNames(ReceptSample[] rs){
+        String[] arr = new String[rs.length];
+        for(int i=0; i<rs.length; i++){
+            arr[i] = rs[i].getName();
         }
+        return arr;
     }
 
+    public Integer[] getImgs(ReceptSample[] rs){
+        Integer[] arr = new Integer[rs.length];
+        for(int i=0; i<rs.length; i++){
+            arr[i] = rs[i].getImg();
+        }
+        return arr;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +58,12 @@ import android.widget.Toast;
 
         myDbHelper = new DatabaseHelper(getApplicationContext());
 
+        LoadResults lr = new LoadResults();
+        lr.execute();
 
 
 
-        imageId[0] = R.drawable.popara;
-        imageId[1] = R.drawable.omlet;
+
 
         }
 
@@ -76,22 +90,102 @@ import android.widget.Toast;
             return super.onOptionsItemSelected(item);
         }
 
+    private class LoadResults extends AsyncTask<Void, Object, ReceptSample[]> {
         DatabaseHelper dbConnector = new DatabaseHelper(
                 getApplicationContext());
-            }
 
+        public LoadResults(){}
+        private boolean isIn(int value, int[] array){
+            for(int i=0; i<array.length; i++){
+                if(value == array[i]) return true;
             }
-
-            if(test && !cursor.isAfterLast()) {
-            }
-
+            return false;
         }
 
+        @Override
+        protected ReceptSample[] doInBackground(Void... params) {
 
+            SQLiteDatabase db = myDbHelper.getWritableDatabase();
+            String[] names = new String[10];
+            int recId = 0;
+            int numOfFound = 0;
+            int j=0;
+            Cursor cursor = db.rawQuery("select _id from combo where productId = " + chosen[j], null);
+
+            while(cursor!=null && j<chosen.length) {
+
+                cursor.moveToFirst();
+
+                for (int i = 0; i < count; i++) {
+                    boolean test = true;
+                    while (cursor.moveToNext()) {
+                        recId = cursor.getInt(cursor.getColumnIndex("_id"));
+                    }
+
+                    cursor = db.rawQuery("select productId from combo where _id = " + recId, null);
+                    cursor.moveToFirst();
+                    while (cursor.moveToNext()) {
+                        if (!isIn(cursor.getInt(cursor.getColumnIndex("productId")), chosen))
+                            test = false;
+                    }
+                    cursor = db.rawQuery("select recName from recipes where _id = " + recId, null);
+
+                    if (test && !cursor.isAfterLast()) {
+                        names[numOfFound] = cursor.getString(cursor.getColumnIndex("recName"));
+                        numOfFound++;
+                    }
+
+                }
+                cursor = db.rawQuery("select _id from combo where productId = " + chosen[j], null);
+                j++;
+            }
+            ReceptSample[] rs = new ReceptSample[numOfFound];
+            for(int i=0; i<numOfFound; i++){
+                rs[i]= new ReceptSample(names[i]) {
+                    @Override
+                    public void setName(String name) {
+                        super.setName(name);
+                    }
+                };
+                Cursor c = db.rawQuery("select * from recipes where recName = " + names[i], null);
+                rs[i].setId(c.getInt(c.getColumnIndex("_id")));
+                rs[i].setDescription(c.getString(c.getColumnIndex("recDescr")));
+                switch(rs[i].getName()) {
+                    case "Омлет":
+                        rs[i].setImg(R.drawable.omlet);
+                        break;
+                    case "Попара":
+                        rs[i].setImg(R.drawable.popara);
+                }
 
             }
 
+
+
+            return  rs;
+        }
+
+        @Override
+        protected void onPostExecute(ReceptSample[] result) {
+            CustomListview adapter = new
+                    CustomListview(ReceptActivity.this, getNames(result), getImgs(result));
+            listView=(ListView)findViewById(R.id.list);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    Toast.makeText(ReceptActivity.this, "You Clicked at " + getNames(recipes)[position], Toast.LENGTH_SHORT).show();
+
+                }
+            });
+                dbConnector.close();
+
+        }
     }
 
 
 }
+
+
